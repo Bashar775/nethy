187a50b0a17760717773b24e4e5693f5b6db0675
Adones <adonesshpep@gmail.com>
2026-01-17 19:26:20 +0300

    user name in order

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -17,6 +18,7 @@ class OrderResource extends JsonResource
         return [
             'id' => $this->id,
             'user_id' => $this->user_id,
+            'user_name'=>User::find($this->user_id)??'user not found',
             'order_number' => $this->order_number,
             'number_of_items' => $this->number_of_items,
             'subtotal' => $this->subtotal,
