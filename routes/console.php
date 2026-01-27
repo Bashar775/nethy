88327afc94a2b111be8a2b0b337cd9c7323a3f31
Adones <adonesshpep@gmail.com>
2026-01-27 20:27:10 +0300

    sdjfskd

--- routes/console.php
+++ routes/console.php
@@ -7,5 +7,5 @@ use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-Schedule::command('backup:run')->daily()->at('17:20'); // Run backup daily at 1:00 AM
+Schedule::command('backup:run')->daily()->at('17:30'); // Run backup daily at 1:00 AM
 Schedule::command('backup:clean')->daily()->at('01:30'); // Run cleanup daily shortly after
